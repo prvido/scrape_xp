8aabb760e5d07d99abcf5780d76cffe047d0bc5d
prvid <xprvidox@gmail.com>
2022-05-29 16:19:53 -0300

    Adicionado a exportação dos dados para um csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from scrape_xp.spiders.stock_spider import StockSpider
 from scrapy.crawler import CrawlerProcess
+import pandas as pd
 
 stock_process = CrawlerProcess(
                     settings = {
@@ -17,3 +18,7 @@ except FileNotFoundError:
 
 stock_process.crawl(StockSpider)
 stock_process.start()
+
+dados = pd.read_json(r'C:\Users\prvid\code\scrapy\scrape_xp\stocks.json', encoding='utf-8-sig')
+
+dados.to_csv(r'C:\Users\prvid\code\scrapy\scrape_xp\stocks.csv', sep=';', encoding='utf-8-sig', decimal=',')
